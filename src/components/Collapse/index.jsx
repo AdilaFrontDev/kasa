9da84901d86e3a3c3347fa491c98c55c2a7594fa
AdilaFrontDev <adila.ambert@hotmail.com>
2024-04-03 15:23:54 +0200

    mise en place de l'animation collapse il reste la rotation de la fleche à faire et la mise en forme du header après click

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,17 +1,24 @@
 import '../../utiles/styles/App.scss'
 
+import { useState } from 'react';
+
 import iconCollapse from '../../assets/collapse-icon.png'
 
 function Collapse({title, content}) {
+    const [isActive, setActive] = useState("false");
+
+    function toogleCollapse(){
+        setActive(!isActive)   
+    }
     return(
         <div className='Collapse'>
             <div className='Collapse__item'>
                 <p className='Collapse__item-text'>{title}</p>
-                <img className='Collapse__item-icon 'src={iconCollapse} alt="Icone collapse"/>
+                <img className='Collapse__item-icon 'src={iconCollapse} alt="Icone collapse" onClick={toogleCollapse}/>
             </div>
-            <div className='Collapse-text'>
+            <div className={isActive ? 'Collapse-text--hiden' : 'Collapse-text'}>
                 {content.map((element) => (
-                    <p className='Collapse-text-p' key={content.indexOf(element)}>{element}</p>
+                    <p className='Collapse-text-p ' key={content.indexOf(element)}>{element}</p>
                 ))}
             </div>
         </div>
