262efefd740d2689c68d35c17c6f576d0a69cad2
AdilaFrontDev <adila.ambert@hotmail.com>
2024-04-04 12:16:07 +0200

    finalisation des animations pour le composant collapse

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,11 +1,15 @@
 import '../../utiles/styles/App.scss'
+import React from 'react'
 
-import { useState } from 'react';
+import { useState } from 'react'
 
 import iconCollapse from '../../assets/collapse-icon.png'
+import { keyframes } from 'styled-components'
+
+
 
 function Collapse({title, content}) {
-    const [isActive, setActive] = useState("false");
+    const [isActive, setActive] = useState("false")
 
     function toogleCollapse(){
         setActive(!isActive)   
@@ -14,11 +18,11 @@ function Collapse({title, content}) {
         <div className='Collapse'>
             <div className='Collapse__item'>
                 <p className='Collapse__item-text'>{title}</p>
-                <img className='Collapse__item-icon 'src={iconCollapse} alt="Icone collapse" onClick={toogleCollapse}/>
+                <img className={isActive ?'Collapse__item-icon' : 'Collapse__item-icon turnDown'} src={iconCollapse} alt="Icone collapse" onClick={toogleCollapse}/>
             </div>
-            <div className={isActive ? 'Collapse-text--hiden' : 'Collapse-text'}>
+            <div className={isActive ? ' Collapse-text hidden' : 'Collapse-text show '}>
                 {content.map((element) => (
-                    <p className='Collapse-text-p ' key={content.indexOf(element)}>{element}</p>
+                    <p className={isActive ? 'Collapse-text-p ' : 'Collapse-text-p'} key={content.indexOf(element)}>{element}</p>
                 ))}
             </div>
         </div>
@@ -26,3 +30,4 @@ function Collapse({title, content}) {
 }
 
 export default Collapse
+
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,30 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
+
 import Logo from '../../assets/logo-kasa-light.png'
 import '../../utiles/styles/App.scss'
 
 function Header() {
+    const [isActiveHome, setActiveHome] = useState("false");
+    const [isActiveAPropos, setActiveAPropos] = useState("false")
+    
+    function toogleHome(){
+        setActiveHome(!isActiveHome)
+        setActiveAPropos("false")   
+    }
+    function toogleAPropos(){
+        setActiveAPropos(!isActiveAPropos)
+        setActiveHome("false")
+    }
+
     return(
         <div className='Header'>
             <Link to="/">
-                            <img className='Header__logo' src={Logo} alt="Logo de Kasa"/>
+                <img className='Header__logo' src={Logo} alt="Logo de Kasa"/>
             </Link>
             <nav className='Header__nav'>
-                <Link to="/" className='Header__nav__Link'>Accueil</Link>
-                <Link to="/APropos" className='Header__nav__Link'>A Propos</Link>
+                <Link to="/" className={isActiveHome ? `Header__nav__Link` : `Header__nav__Link--active` } onClick={toogleHome}>Accueil</Link>
+                <Link to="/APropos" className={isActiveAPropos? `Header__nav__Link` : `Header__nav__Link--active` } onClick={toogleAPropos}>A Propos</Link>
             </nav>
         </div>
     )
